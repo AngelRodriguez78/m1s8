92cc4e5fee41a8590307ea0ea33a03dfc63b4f47
Angel Daniel RM <119967462+AngelRodriguez78@users.noreply.github.com>
2023-02-02 22:41:58 -0500

    Sumas clases

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,105 +1,28 @@
 console.log("Entro index.js");
+// convertimos el JSON a objeto de javascript
+let objStudents = JSON.parse(db);
 
-let mascota = {
-    nombre: "Firulais",
-    edad: 4,
-    color: "Cafe",
-    raza: "Labrador",
-    peso: 8,
-    vacunas: ["Vacuna 1", "Vacuna 2"]
-}
+//a partir de este punto ya se trabaja como cualquier objeto
 
-console.log(mascota.nombre);
-console.log(mascota.edad);
-console.log(mascota);
-console.log(mascota.vacunas[0]);
-console.log(mascota["vacunas"][1]);
-console.log(mascota.vacunas);
+console.log(db);
+console.log(objStudents.students);
 
-const colors = [
-    { name: 'Red', code: '#ff0000' },
-    { name: 'Green', code: '#00ff00' },
-    { name: 'Blue', code: '#0000ff' },
-    { name: 'Yellow', code: '#ffff00' },
-    { name: 'Purple', code: '#800080' }
-]
-
-// console.log(colors[2]);
-
-colors[1].spanish ="verde"
-console.log(colors[1].spanish);
-console.log(colors);
-
-// quiero una funcion que reciba un arreglo(?) e imprima el arreglo, 
-// filtrando o buscando el color mandado//
-
-function filtrarColor(arreglo, color){
-    let arregloFiltrado = arreglo.find(item =>{
-       return item.name == color;
-    });
-
-    return arregloFiltrado;
-}
-
-console.log(filtrarColor(colors,"Blue"),"colores filtrados");
+objStudents.students.forEach(student => {
+    let sumGrades = student.grades.reduce((total, grade) => {return total + grade}, 0);
+    student.average = sumGrades /3;
+    console.log(student);
+});
 
 
-let employeesWithSalary = [
-    {
-        name: "Jesus Cardenas",
-        age: 32,
-        position: "Manager",
-        salary: 500
-    },
-    {
-        name: "Manuel Martinez",
-        age: 27,
-        position: "Developer",
-        salary: 300
-    },
-    {
-        name: "Carlos Perez",
-        age: 27,
-        position: "Developer",
-        salary: 300
-    },
-    {
-        name: "Arturo Lopez",
-        age: 27,
-        position: "Developer",
-        salary: 300
-    },
-    {
-        name: "Juan Hernandez",
-        age: 27,
-        position: "Developer",
-        salary: 300
-    },
-    {
-        name: "Bob Johnson",
-        age: 35,
-        position: "Designer",
-        salary: 300
-    },
-    {
-        name: "Emily Davis",
-        age: 29,
-        position: "QA Engineer",
-        salary: 300
-    },
-    {
-        name: "William Brown",
-        age: 31,
-        position: "Product Manager",
-        salary: 300
-    }
-];
+let  miCalculadora = new Calculadora();
 
-function filtrarTrabajo(empleados){
-    let arregloFiltrado2 = empleados.filter(empleado => {
-        return empleado.position == "Developer"
-    });
-    return arregloFiltrado2;
-}
+console.log(miCalculadora.sumar(2,2),"Suma");
+console.log(miCalculadora.restar(5,2), "Resta");
+console.log(miCalculadora.multiplicar(5,2), "Multiplicar");
+console.log(miCalculadora.dividir(5,2), "Dividir");
 
-console.log (filtrarTrabajo(employeesWithSalary))
+let miCalculadoraCientifica = new CalculadoraCientifica();
+
+console.log(miCalculadoraCientifica.sumar(3,3), "suma desde cientifica");
+console.log(miCalculadoraCientifica.raizCuadrada(9), "Raiz Cuadrada desde cientifica");
+
